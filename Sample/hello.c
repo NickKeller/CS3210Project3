45a7c50f98d7bf9aafdaf960bff9cf93c0275388
nkeller3 <nick.keller57@gmail.com>
2015-04-07 18:57:48 -0400

    Added file operation functions, but I haven't figured out how the permissions work

--- Sample/hello.c
+++ Sample/hello.c
@@ -10,14 +10,23 @@
 
 #define FUSE_USE_VERSION 26
 
-#include <fuse.h>
-#include <stdio.h>
-#include <string.h>
+#include <ctype.h>
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <fuse.h>
+#include <libgen.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 
 static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/hello";
+static const char *hello_path = "/hello.txt";
+
 
 static int hello_getattr(const char *path, struct stat *stbuf)
 {
@@ -86,14 +95,66 @@ static int hello_read(const char *path, char *buf, size_t size, off_t offset,
 	return size;
 }
 
+static int hello_create(const char *path, mode_t mode, struct fuse_file_info *fi){
+	int retstat = 0;
+	int fd = creat(path, mode);
+	if(fd < 0){
+		retstat = -errno;
+	}
+	
+	fi->fh = fd;
+	
+	return retstat;
+
+}
+
+static int hello_unlink(const char *path){
+	int retstat = 0;
+	retstat = unlink(path);
+	if(retstat < 0){
+		retstat = -errno;
+	}
+	
+	return retstat;
+}
+
+static void hello_destroy(void *userdata){
+	
+}
+
+static int hello_rename(const char *path, const char *newpath){
+	int retstat = rename(path,newpath);
+	if(retstat < 0)
+		retstat = -errno;
+	return retstat;
+}
+
+/** Change the permission bits of a file */
+int hello_chmod(const char *path, mode_t mode)
+{
+    int retstat = chmod(path, mode);
+    if (retstat < 0)
+	retstat = -errno;
+    
+    return retstat;
+}
+
+
+
 static struct fuse_operations hello_oper = {
 	.getattr	= hello_getattr,
 	.readdir	= hello_readdir,
 	.open		= hello_open,
 	.read		= hello_read,
+	.create		= hello_create,
+	.unlink		= hello_unlink,
+	.destroy	= hello_destroy,
+	.rename		= hello_rename,
+	.chmod		= hello_chmod
 };
 
 int main(int argc, char *argv[])
 {
+	printf("PATH_MAX is:%d\n",PATH_MAX);
 	return fuse_main(argc, argv, &hello_oper, NULL);
 }
