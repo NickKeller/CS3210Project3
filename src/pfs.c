b2e0ed452f306f8cfba72176894603b54904753b
nkeller3 <nick.keller57@gmail.com>
2015-04-18 21:24:55 -0400

    Renamed to pfs

--- src/pfs.c
+++ src/pfs.c
@@ -5,12 +5,12 @@
   This program can be distributed under the terms of the GNU GPL.
   See the file COPYING.
 
-  gcc -Wall hello.c `pkg-config fuse --cflags --libs` -o hello
+  gcc -Wall pfs.c `pkg-config fuse --cflags --libs` -o pfs
 */
 
 #define FUSE_USE_VERSION 26
 
-#include "hello.h"
+#include "pfs.h"
 
 #include <ctype.h>
 #include <dirent.h>
@@ -30,18 +30,18 @@
 //  have the mountpoint.  I'll save it away early on in main(), and then
 //  whenever I need a path for something I'll call this to construct
 //  it.
-static void hello_fullpath(char fpath[PATH_MAX], const char *path)
+static void pfs_fullpath(char fpath[PATH_MAX], const char *path)
 {
     strcpy(fpath, PRI_DATA->rootdir);
     strncat(fpath, path, PATH_MAX);
 }
 
 
-static int hello_getattr(const char *path, struct stat *stbuf)
+static int pfs_getattr(const char *path, struct stat *stbuf)
 {
 	int retstat = 0;
 	char fpath[PATH_MAX];
-	hello_fullpath(fpath,path);
+	pfs_fullpath(fpath,path);
 	
 	retstat = lstat(fpath,stbuf);
 	if(retstat != 0){
@@ -50,11 +50,11 @@ static int hello_getattr(const char *path, struct stat *stbuf)
 	return retstat;
 }
 
-int hello_readlink(const char* path, char* link, size_t size){
+int pfs_readlink(const char* path, char* link, size_t size){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath, path);
+	pfs_fullpath(fpath, path);
 	
 	
 	retstat = readlink(fpath, link, size - 1);
@@ -65,11 +65,11 @@ int hello_readlink(const char* path, char* link, size_t size){
 	return retstat;
 }
 
-int hello_mknod(const char* path, mode_t mode, dev_t dev){
+int pfs_mknod(const char* path, mode_t mode, dev_t dev){
 	int retstat;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath,path);
+	pfs_fullpath(fpath,path);
 	
 	if(S_ISREG(mode)){
 		retstat = open(fpath, O_CREAT | O_EXCL | O_WRONLY, mode);
@@ -99,11 +99,11 @@ int hello_mknod(const char* path, mode_t mode, dev_t dev){
 	return retstat;
 }
 
-int hello_mkdir(const char* path, mode_t mode){
+int pfs_mkdir(const char* path, mode_t mode){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath, path);
+	pfs_fullpath(fpath, path);
 	
 	retstat = mkdir(fpath,mode);
 	if(retstat < 0){
@@ -113,11 +113,11 @@ int hello_mkdir(const char* path, mode_t mode){
 	return retstat;
 }
 
-int hello_unlink(const char* path){
+int pfs_unlink(const char* path){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath,path);
+	pfs_fullpath(fpath,path);
 	
 	retstat = unlink(fpath);
 	if(retstat < 0){
@@ -127,11 +127,11 @@ int hello_unlink(const char* path){
 	return retstat;
 }
 
-int hello_rmdir(const char* path){
+int pfs_rmdir(const char* path){
 	  int retstat = 0;
 	  char fpath[PATH_MAX];
 	  
-	  hello_fullpath(fpath, path);
+	  pfs_fullpath(fpath, path);
 	  
 	  retstat = rmdir(fpath);
 	  if(retstat < 0){
@@ -141,12 +141,12 @@ int hello_rmdir(const char* path){
 	  return retstat;
 }
 
-int hello_symlink(const char *path, const char *link)
+int pfs_symlink(const char *path, const char *link)
 {
     int retstat = 0;
     char flink[PATH_MAX];
     
-	hello_fullpath(flink, link);    
+	pfs_fullpath(flink, link);    
 	
     retstat = symlink(path, flink);
     if (retstat < 0){
@@ -155,13 +155,13 @@ int hello_symlink(const char *path, const char *link)
     return retstat;
 }
 
-int hello_rename(const char* path, const char* newpath){
+int pfs_rename(const char* path, const char* newpath){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	char fnewpath[PATH_MAX];
 	
-	hello_fullpath(fpath,path);
-	hello_fullpath(fnewpath, newpath);
+	pfs_fullpath(fpath,path);
+	pfs_fullpath(fnewpath, newpath);
 	
 	retstat = rename(fpath, fnewpath);
 	if(retstat < 0){
@@ -171,12 +171,12 @@ int hello_rename(const char* path, const char* newpath){
 	return retstat;
 }
 
-int hello_link(const char* path, const char* newpath){
+int pfs_link(const char* path, const char* newpath){
 	int retstat = 0;
 	char fpath[PATH_MAX], fnewpath[PATH_MAX];
 	
-	hello_fullpath(fpath,path);
-	hello_fullpath(fnewpath,newpath);
+	pfs_fullpath(fpath,path);
+	pfs_fullpath(fnewpath,newpath);
 	
 	retstat = link(fpath, fnewpath);
 	if(retstat < 0){
@@ -187,12 +187,12 @@ int hello_link(const char* path, const char* newpath){
 }
 
 /** Change the permission bits of a file */
-int hello_chmod(const char *path, mode_t mode)
+int pfs_chmod(const char *path, mode_t mode)
 {
     int retstat;
     char fpath[PATH_MAX];
     
-    hello_fullpath(fpath,path);
+    pfs_fullpath(fpath,path);
     retstat = chmod(path, mode);
     if (retstat < 0)
 	retstat = -errno;
@@ -200,11 +200,11 @@ int hello_chmod(const char *path, mode_t mode)
     return retstat;
 }
 
-int hello_chown(const char* path, uid_t uid, gid_t gid){
+int pfs_chown(const char* path, uid_t uid, gid_t gid){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath,path);
+	pfs_fullpath(fpath,path);
 	
 	retstat = chown(fpath, uid, gid);
 	if(retstat < 0){
@@ -214,11 +214,11 @@ int hello_chown(const char* path, uid_t uid, gid_t gid){
 	return retstat;
 }
 
-int hello_truncate(const char* path, off_t newsize){
+int pfs_truncate(const char* path, off_t newsize){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath, path);
+	pfs_fullpath(fpath, path);
 	
 	retstat = truncate(fpath, newsize);
 	if(retstat < 0) retstat = -errno;
@@ -226,11 +226,11 @@ int hello_truncate(const char* path, off_t newsize){
 	return retstat;
 }
 
-int hello_utime(const char* path, struct utimbuf *ubuf){
+int pfs_utime(const char* path, struct utimbuf *ubuf){
 	int retstat;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath,path);
+	pfs_fullpath(fpath,path);
 	
 	retstat = utime(fpath,ubuf);
 	if(retstat < 0) retstat = -errno;
@@ -238,13 +238,13 @@ int hello_utime(const char* path, struct utimbuf *ubuf){
 	return retstat;
 }
 
-static int hello_open(const char *path, struct fuse_file_info *fi)
+static int pfs_open(const char *path, struct fuse_file_info *fi)
 {
 	int retstat = 0;
 	int fd;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath, path);
+	pfs_fullpath(fpath, path);
 	
 	fd = open(fpath, fi->flags);
 	if(fd < 0){
@@ -256,7 +256,7 @@ static int hello_open(const char *path, struct fuse_file_info *fi)
 	return retstat;
 }
 
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+static int pfs_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi)
 {
 	int retstat = pread(fi->fh, buf, size, offset);
@@ -265,7 +265,7 @@ static int hello_read(const char *path, char *buf, size_t size, off_t offset,
 	return retstat;
 }
 
-int hello_write(const char* path, const char* buf, size_t size, off_t offset, 
+int pfs_write(const char* path, const char* buf, size_t size, off_t offset, 
 				struct fuse_file_info* fi)
 {
 	 int retstat = 0;
@@ -276,11 +276,11 @@ int hello_write(const char* path, const char* buf, size_t size, off_t offset,
 	 return retstat;				
 }
 
-int hello_statfs(const char* path, struct statvfs* statv){
+int pfs_statfs(const char* path, struct statvfs* statv){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath, path);
+	pfs_fullpath(fpath, path);
 	
 	retstat = statvfs(fpath, statv);
 	if(retstat < 0) retstat = -errno;
@@ -288,19 +288,19 @@ int hello_statfs(const char* path, struct statvfs* statv){
 	return retstat;
 }
 
-int hello_flush(const char* path, struct fuse_file_info* fi){
+int pfs_flush(const char* path, struct fuse_file_info* fi){
 	int retstat = 0;
 	
 	return retstat;
 }
 
-int hello_release(const char* path, struct fuse_file_info* fi){
+int pfs_release(const char* path, struct fuse_file_info* fi){
 	int retstat = 0;
 	retstat = close(fi->fh);
 	return retstat;
 }
 
-int hello_fsync(const char* path, int datasync, struct fuse_file_info* fi){
+int pfs_fsync(const char* path, int datasync, struct fuse_file_info* fi){
 	int retstat = 0;
 #ifdef HAVE_FDATASYNC
 	if(datasync){
@@ -317,12 +317,12 @@ int hello_fsync(const char* path, int datasync, struct fuse_file_info* fi){
 
 #ifdef HAVE_SYS_XATTR_H
 /** Set extended attributes */
-int hello_setxattr(const char *path, const char *name, const char *value, size_t size, int flags)
+int pfs_setxattr(const char *path, const char *name, const char *value, size_t size, int flags)
 {
     int retstat = 0;
     char fpath[PATH_MAX];
     
-    hello_fullpath(fpath, path);
+    pfs_fullpath(fpath, path);
     
     retstat = lsetxattr(fpath, name, value, size, flags);
     if (retstat < 0)
@@ -332,12 +332,12 @@ int hello_setxattr(const char *path, const char *name, const char *value, size_t
 }
 
 /** Get extended attributes */
-int hello_getxattr(const char *path, const char *name, char *value, size_t size)
+int pfs_getxattr(const char *path, const char *name, char *value, size_t size)
 {
     int retstat = 0;
     char fpath[PATH_MAX];
     
-    hello_fullpath(fpath, path);
+    pfs_fullpath(fpath, path);
     
     retstat = lgetxattr(fpath, name, value, size);
     if (retstat < 0)
@@ -347,14 +347,14 @@ int hello_getxattr(const char *path, const char *name, char *value, size_t size)
 }
 
 /** List extended attributes */
-int hello_listxattr(const char *path, char *list, size_t size)
+int pfs_listxattr(const char *path, char *list, size_t size)
 {
     int retstat = 0;
     char fpath[PATH_MAX];
     char *ptr;
     
     
-    hello_fullpath(fpath, path);
+    pfs_fullpath(fpath, path);
     
     retstat = llistxattr(fpath, list, size);
     if (retstat < 0)
@@ -364,12 +364,12 @@ int hello_listxattr(const char *path, char *list, size_t size)
 }
 
 /** Remove extended attributes */
-int hello_removexattr(const char *path, const char *name)
+int pfs_removexattr(const char *path, const char *name)
 {
     int retstat = 0;
     char fpath[PATH_MAX];
     
-    hello_fullpath(fpath, path);
+    pfs_fullpath(fpath, path);
     
     retstat = lremovexattr(fpath, name);
     if (retstat < 0)
@@ -379,12 +379,12 @@ int hello_removexattr(const char *path, const char *name)
 }
 #endif
 
-int hello_opendir(const char* path, struct fuse_file_info* fi){
+int pfs_opendir(const char* path, struct fuse_file_info* fi){
 	DIR *dp;
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath,path);
+	pfs_fullpath(fpath,path);
 	
 	dp = opendir(fpath);
 	if(dp == NULL){
@@ -395,7 +395,7 @@ int hello_opendir(const char* path, struct fuse_file_info* fi){
 	return retstat;
 }
 
-int hello_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, 
+int pfs_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, 
 					struct fuse_file_info* fi)
 {
 	int retstat = 0;
@@ -419,28 +419,28 @@ int hello_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t off
 	return retstat;
 }
 
-int hello_releasedir(const char* path, struct fuse_file_info* fi){
+int pfs_releasedir(const char* path, struct fuse_file_info* fi){
 	int retstat = 0;
 	
 	closedir((DIR*)(uintptr_t)fi->fh);
 	return retstat;
 }
 
-int hello_fsyncdir(const char* path, int datasync, struct fuse_file_info* fi){
+int pfs_fsyncdir(const char* path, int datasync, struct fuse_file_info* fi){
 	return 0;
 }
 
-void* hello_init(struct fuse_conn_info *conn){
+void* pfs_init(struct fuse_conn_info *conn){
 	return PRI_DATA;
 }
 
-void hello_destroy(void* userdata){}
+void pfs_destroy(void* userdata){}
 
-int hello_access(const char* path, int mask){
+int pfs_access(const char* path, int mask){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	
-	hello_fullpath(fpath,path);
+	pfs_fullpath(fpath,path);
 	
 	retstat = access(fpath,mask);
 	
@@ -449,12 +449,12 @@ int hello_access(const char* path, int mask){
 	return retstat;
 }
 
-int hello_create(const char* path, mode_t mode, struct fuse_file_info* fi){
+int pfs_create(const char* path, mode_t mode, struct fuse_file_info* fi){
 	int retstat = 0;
 	char fpath[PATH_MAX];
 	int fd;
 	
-	hello_fullpath(fpath,path);
+	pfs_fullpath(fpath,path);
 	
 	fd = creat(fpath, mode);
 	if(fd < 0) retstat = -errno;
@@ -464,18 +464,18 @@ int hello_create(const char* path, mode_t mode, struct fuse_file_info* fi){
 	return retstat;
 }
 
-int hello_ftruncate(const char* path, off_t offset, struct fuse_file_info* fi){
+int pfs_ftruncate(const char* path, off_t offset, struct fuse_file_info* fi){
 	int retstat = 0;
 	retstat = ftruncate(fi->fh, offset);
 	if(retstat < 0) retstat = -errno;
 	return retstat;
 }
 
-int hello_fgetattr(const char* path, struct stat* statbuf, struct fuse_file_info* fi){
+int pfs_fgetattr(const char* path, struct stat* statbuf, struct fuse_file_info* fi){
 	int retstat = 0;
 	
 	if(!strcmp(path,"/")){
-		return hello_getattr(path,statbuf);
+		return pfs_getattr(path,statbuf);
 	}
 	
 	retstat = fstat(fi->fh, statbuf);
@@ -486,48 +486,48 @@ int hello_fgetattr(const char* path, struct stat* statbuf, struct fuse_file_info
 
 
 
-struct fuse_operations hello_oper = {
-  .getattr = hello_getattr,
-  .readlink = hello_readlink,
+struct fuse_operations pfs_oper = {
+  .getattr = pfs_getattr,
+  .readlink = pfs_readlink,
   // no .getdir -- that's deprecated
   .getdir = NULL,
-  .mknod = hello_mknod,
-  .mkdir = hello_mkdir,
-  .unlink = hello_unlink,
-  .rmdir = hello_rmdir,
-  .symlink = hello_symlink,
-  .rename = hello_rename,
-  .link = hello_link,
-  .chmod = hello_chmod,
-  .chown = hello_chown,
-  .truncate = hello_truncate,
-  .utime = hello_utime,
-  .open = hello_open,
-  .read = hello_read,
-  .write = hello_write,
+  .mknod = pfs_mknod,
+  .mkdir = pfs_mkdir,
+  .unlink = pfs_unlink,
+  .rmdir = pfs_rmdir,
+  .symlink = pfs_symlink,
+  .rename = pfs_rename,
+  .link = pfs_link,
+  .chmod = pfs_chmod,
+  .chown = pfs_chown,
+  .truncate = pfs_truncate,
+  .utime = pfs_utime,
+  .open = pfs_open,
+  .read = pfs_read,
+  .write = pfs_write,
   /** Just a placeholder, don't set */ // huh???
-  .statfs = hello_statfs,
-  .flush = hello_flush,
-  .release = hello_release,
-  .fsync = hello_fsync,
+  .statfs = pfs_statfs,
+  .flush = pfs_flush,
+  .release = pfs_release,
+  .fsync = pfs_fsync,
   
 #ifdef HAVE_SYS_XATTR_H
-  .setxattr = hello_setxattr,
-  .getxattr = hello_getxattr,
-  .listxattr = hello_listxattr,
-  .removexattr = hello_removexattr,
+  .setxattr = pfs_setxattr,
+  .getxattr = pfs_getxattr,
+  .listxattr = pfs_listxattr,
+  .removexattr = pfs_removexattr,
 #endif
   
-  .opendir = hello_opendir,
-  .readdir = hello_readdir,
-  .releasedir = hello_releasedir,
-  .fsyncdir = hello_fsyncdir,
-  .init = hello_init,
-  .destroy = hello_destroy,
-  .access = hello_access,
-  .create = hello_create,
-  .ftruncate = hello_ftruncate,
-  .fgetattr = hello_fgetattr
+  .opendir = pfs_opendir,
+  .readdir = pfs_readdir,
+  .releasedir = pfs_releasedir,
+  .fsyncdir = pfs_fsyncdir,
+  .init = pfs_init,
+  .destroy = pfs_destroy,
+  .access = pfs_access,
+  .create = pfs_create,
+  .ftruncate = pfs_ftruncate,
+  .fgetattr = pfs_fgetattr
 };
 
 int main(int argc, char *argv[])
@@ -545,7 +545,7 @@ int main(int argc, char *argv[])
 	argc--;
 	
 	printf("PATH_MAX is:%d\n",PATH_MAX);
-	fuse_stat = fuse_main(argc, argv, &hello_oper, data);
+	fuse_stat = fuse_main(argc, argv, &pfs_oper, data);
 	return fuse_stat;
 }
 
